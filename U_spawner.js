caffb05f1a5fe8692a0a6f2a1b58d4acd4ee140e
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-07 23:27:54 +0800

    7/7/2020:2327

--- U_spawner.js
+++ U_spawner.js
@@ -17,8 +17,3 @@ module.exports = {
 	  }
   }
 };
-
-Game.spawns['Boopy1'].spawnCreep([WORK, CARRY, MOVE], 'H1', { memory: {role: 'harvester'} });
-Game.spawns['Boopy1'].spawnCreep([WORK, CARRY, MOVE], 'B1', { memory: {role: 'builder'} });
-Game.spawns['Boopy1'].spawnCreep([WORK, CARRY, MOVE], 'B2', { memory: {role: 'builder'} });
-Game.spawns['Boopy1'].spawnCreep([WORK, CARRY, MOVE], 'B3', { memory: {role: 'builder'} });
