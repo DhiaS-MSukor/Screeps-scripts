7ed33133ed4fbc5e99f0a1ec6334e781fe40dc57
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-08 14:28:10 +0800

    less builders

--- U_creeps.js
+++ U_creeps.js
@@ -5,7 +5,7 @@ var r_repairer = require('R_repairer');
 var r_runner = require('R_runner');
 
 var creeps = ['H1','H2','H3',
-              'B1','B2','B3','B4','B5','B6','B7','B8','B9','B10',
+              'B1','B2','B3','B4','B5',
               'R1', 'R2', 'R3',
               'r1'];
 
