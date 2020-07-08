3a1d31d59f472a710e856960b864e6250b8b8eef
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-08 11:46:05 +0800

    color change

--- R_builder.js
+++ R_builder.js
@@ -16,7 +16,7 @@ module.exports = {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffeeff'}});
                 }
             }
 
@@ -30,7 +30,7 @@ module.exports = {
 	    else {
 	        var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa11'}});
             }
 	    }
 	}
