0426d8bf6df808726d91d340fbcbdee59b089858
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-08 14:21:16 +0800

    runner fix

--- R_runner.js
+++ R_runner.js
@@ -15,14 +15,14 @@ module.exports = {
 	    if(creep.memory.building) {
 			var targets = creep.room.find(FIND_MY_CREEPS, {filter: (creep) => {return (creep.memory.role == 'builder')}});
 			for (var i in targets) {
-				if (targets[i].store[RESOURCE_ENERGY] <= 50 && creep.transfer(targets[i], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				if (targets[i].store[RESOURCE_ENERGY] < 50 && creep.transfer(targets[i], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(targets[i], {visualizePathStyle: {stroke: '#ffeeee'}}); 
 					return;
 				}
 			}
 			var targets = creep.room.find(FIND_MY_CREEPS, {filter: (creep) => {return (creep.memory.role == 'repairer')}});
 			for (var i in targets) {
-				if (targets[i].store[RESOURCE_ENERGY] <= 50 && creep.transfer(targets[i], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				if (targets[i].store[RESOURCE_ENERGY] < 50 && creep.transfer(targets[i], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(targets[i], {visualizePathStyle: {stroke: '#ffeeee'}}); 
 					return;
 				}
