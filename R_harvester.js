3decf92b753dda0e7261ea5d9029e14790df74b4
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-08 13:24:06 +0800

    harvester prioritizes spawn

--- R_harvester.js
+++ R_harvester.js
@@ -12,18 +12,29 @@ module.exports = {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION || 
-                                structure.structureType == STRUCTURE_SPAWN || 
-                                structure.structureType == STRUCTURE_CONTAINER) &&
+                                structure.structureType == STRUCTURE_SPAWN) &&
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
+
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    return;
                 }
+            }
 
-                else if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_CONTAINER) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+            });
+
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    return;
                 }
             }
         }
