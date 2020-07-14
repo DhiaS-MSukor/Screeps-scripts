df7d50fade036c72e7900b269fa1641ba8e9c257
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-14 11:34:45 +0800

    minor fix

--- R_harvester.js
+++ R_harvester.js
@@ -29,44 +29,31 @@ module.exports = {
             }
         }
         else {
-
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
+            var targets = creep.room.find(FIND_STRUCTURES, { filter: (structure) => {
                         return (structure.structureType == STRUCTURE_TOWER) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
+                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;}
             });
 
             if (doTransfer(targets)) {
                 return;
             }
 
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
+            var targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {
                         return (structure.structureType == STRUCTURE_CONTAINER) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
+                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;}
             });
 
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                    return;
-                }
+            if (doTransfer(targets)) {
+                return;
             }
              
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
+            var targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {
                         return (structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
+                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;}
             });
 
-            if (targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                    return;
-                }
+            if (doTransfer(targets)) {
+                return;
             }
 
         }
