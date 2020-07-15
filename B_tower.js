3980639d4322afc43b325372a29b8fdc95925e8b
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-15 18:41:04 +0800

    added tower role

---
+++ B_tower.js
@@ -0,0 +1,22 @@
+// JavaScript source code 
+
+module.exports = {
+	run: function() {
+		var tower = Game.getObjectById('5f0890983513bc4000713369');
+
+        if(tower) {
+            var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+            });
+
+            if(closestDamagedStructure) {
+                tower.repair(closestDamagedStructure);
+            }
+
+            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            if(closestHostile) {
+                tower.attack(closestHostile);
+            }
+        } 
+	}
+}
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var u_creeps = require('U_creeps');
 var u_misc = require('U_misc');
+var b_tower = require('B_tower');
 
 module.exports.loop = function () {
 	u_misc.run();
     u_creeps.run();
+    b_tower.run();
 }
