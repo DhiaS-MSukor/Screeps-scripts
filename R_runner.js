49580caa41b6af4139941f513a6b4978c92c0c06
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-16 20:41:47 +0800

    runner pick up energy from tombstones

--- R_runner.js
+++ R_runner.js
@@ -52,6 +52,14 @@ module.exports = {
 				}
 			} 
 
+			targets = creep.room.find(FIND_TOMBSTONES, {filter: (targets) => { return (targets.store[RESOURCE_ENERGY] != 0)}});
+			if (targets.length) {
+				if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff0022'}});
+					return;
+				}
+			} 
+
 			targets = creep.room.find(FIND_STRUCTURES, {filter: (targets) => { return (targets.structureType == STRUCTURE_CONTAINER && 
 																						   targets.store[RESOURCE_ENERGY] != 0)}});
 			if (targets.length) {
