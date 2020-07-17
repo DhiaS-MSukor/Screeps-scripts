81ebcb9a040e113caff7712e08d1757853f2100a
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-18 00:19:52 +0800

    runner optimization

--- R_runner.js
+++ R_runner.js
@@ -9,6 +9,16 @@ var doTransfer = function(targets, creep) {
 	return false;
 }
 
+var doWithdraw = function(creep, targets) {
+	if (targets.length) {
+		if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+			creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff0022'}});  
+			return true;
+		} 
+	}
+	return false;
+}
+
 module.exports = {
     /** @param {Creep} creep **/
     run: function(creep) {
@@ -52,19 +62,18 @@ module.exports = {
 				}
 			} 
 
+			targets = creep.room.find(FIND_RUINS, {filter: (targets) => { return (targets.store[RESOURCE_ENERGY] != 0)}});
+			if (doWithdraw(creep, targets)) {return;} 
+
+			targets = creep.room.find(FIND_TOMBSTONES, {filter: (targets) => { return (targets.store[RESOURCE_ENERGY] != 0)}});
+			if (doWithdraw(creep, targets)) {return;} 
+
 			targets = creep.room.find(FIND_STRUCTURES, {filter: (targets) => { return (targets.structureType == STRUCTURE_CONTAINER && 
 																						   targets.store[RESOURCE_ENERGY] != 0)}});
-			if (targets.length) {
-				if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff0022'}});
-					return;
-				}
-			}
+			if (doWithdraw(creep, targets)) {return;} 
 
 	        targets = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(targets[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff0022'}});
-            }
+			if (doWithdraw(creep, targets)) {return;} 
 	    }
 	}
 };
