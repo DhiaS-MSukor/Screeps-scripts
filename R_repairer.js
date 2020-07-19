d117e9a9b318f82202d9b672e66b84f6e8458ed4
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-19 21:57:18 +0800

    do caching for repairer

--- R_repairer.js
+++ R_repairer.js
@@ -1,9 +1,20 @@
+var doRepair = function(creep, targets) {
+	if (targets.length) {
+		creep.memory.repairTarget = targets[0].id;
+		if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+			creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffff00'}});  
+		}
+		return true;
+	}
+	return false;
+}
 
 module.exports = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
 		var targets;
+		var cache;
 
 	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.building = false;
@@ -15,24 +26,31 @@ module.exports = {
 	    }
 
 	    if(creep.memory.building) { 
-			targets = creep.room.find(FIND_MY_STRUCTURES, {filter: (structure) => { return (structure.hits < structure.hitsMax)}});
-			if (targets.length) {
-				if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffff00'}}); 
-					return;
-				}
+			cache = Game.getObjectById(creep.memory.repairTarget);
+			if (cache) {
+				if (cache.hits < cache.hitsMax) {
+					if(doRepair(creep, [cache])) {return;} 
+				} 
 			}
 
+			targets = creep.room.find(FIND_MY_STRUCTURES, {filter: (structure) => { return (structure.hits < structure.hitsMax)}});
+			if(doRepair(creep, targets)) {return;} 
+
 			targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => { return (structure.hits < structure.hitsMax)}});
-			if (targets.length) {
-				if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffff00'}}); 
-					return;
-				}
-			}
+			if(doRepair(creep, targets)) {return;} 
 	    }
 
 	    else {
+			cache = Game.getObjectById(creep.memory.repairTarget);
+			if (cache) {
+				if (cache.store[RESOURCE_ENERGY] != 0) {
+					if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+						creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffff00'}});
+						return;
+					}
+				}
+			}
+
 			targets = creep.room.find(FIND_STRUCTURES, {filter: (structure) => { return (structure.structureType == STRUCTURE_CONTAINER &&
 																							 structure.store[RESOURCE_ENERGY] != 0)}});
 			if (targets.length) {
