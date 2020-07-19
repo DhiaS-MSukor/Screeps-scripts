0f20cb6ec83a6520802b95c2bb3f5971e62176f3
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-19 22:28:36 +0800

    fix

--- R_runner.js
+++ R_runner.js
@@ -1,6 +1,6 @@
 // JavaScript source code
 var doTransfer = function(targets, creep) {
-	if (targets.length > 0) {
+	if (targets.length) {
 		creep.memory.target = targets[0].id;
 		if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
 			creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff00ff'}});  
@@ -11,7 +11,7 @@ var doTransfer = function(targets, creep) {
 }
 
 var doWithdraw = function(creep, targets) {
-	if (targets.length > 0) {
+	if (targets.length) {
 		creep.memory.source = targets[0].id;
 		if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
 			creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff00ff'}});  
@@ -92,8 +92,10 @@ module.exports = {
 			}
 
 			targets = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (targets) => { return (targets.structureType == STRUCTURE_CONTAINER && 
-																						   targets.store[RESOURCE_ENERGY] != 0)}});
-			if (doWithdraw(creep, [targets])) {return;}  
+																						   targets.store[RESOURCE_ENERGY] != 0)}}); 
+			if (targets) {
+				if (doWithdraw(creep, [targets])) {return;}  
+			} 
 	    }
 	}
 };
