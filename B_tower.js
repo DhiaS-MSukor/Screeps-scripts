0a0ee33db2c5335f26c5380fb0b0d1f4635c962f
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-20 10:37:06 +0800

    tower fix

--- B_tower.js
+++ B_tower.js
@@ -1,28 +1,31 @@
 // JavaScript source code 
 
+var doRole = function(tower) {
+    if (tower) { 
+        var target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(target) {
+            tower.attack(target);
+            return;
+        }
+
+        target = tower.room.find(FIND_MY_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax}); 
+        if(target.length) {
+            tower.repair(target[0]);
+            return;
+        }
+
+        target = tower.room.find(FIND_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax}); 
+        if(target.length) {
+            tower.repair(target[0]);
+            return;
+        }
+    } 
+}
+
 module.exports = {
 	run: function() {
-        var target;
-		var tower = Game.getObjectById('5f0890983513bc4000713369');
-
-        if(tower) { 
-            target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            if(target) {
-                tower.attack(target);
-                return;
-            }
-
-            target = tower.room.find(FIND_MY_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax}); 
-            if(target.length) {
-                tower.repair(target[0]);
-                return;
-            }
-
-            target = tower.room.find(FIND_STRUCTURES, {filter: (structure) => structure.hits < structure.hitsMax}); 
-            if(target.length) {
-                tower.repair(target[0]);
-                return;
-            }
-        } 
+        for (var name in Memory.towers) {
+            doRole(Game.getObjectById(Memory.towers[name]));  
+		}
 	}
 }
