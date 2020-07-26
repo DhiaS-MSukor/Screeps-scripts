07b45ddb97f729523f94c727034f858c1e01b473
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-26 23:58:51 +0800

    skip auto spawn if spawn energy < 300

--- U_creeps.js
+++ U_creeps.js
@@ -74,7 +74,7 @@ var auto_respawn = function(){
     var name;
     var spawn = Memory.mainSpawn;
 
-    if (spawn.spawning) {return;}
+    if (spawn.store[RESOURCE_ENERGY] < 300 || spawn.spawning) {return;}
 
     if (spawn_check(spawn, 'harvester', 1)) {return;}
     else if (spawn_check(spawn, 'builder', 1)) {return;}
