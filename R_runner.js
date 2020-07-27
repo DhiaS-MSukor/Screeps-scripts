0fad16f2b3b2bdfcf35dedfe6d0efb7da801f3f9
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-27 22:58:19 +0800

    runner stores other resources to containers

--- R_runner.js
+++ R_runner.js
@@ -1,7 +1,7 @@
 // JavaScript source code
-var doTransfer = function(targets, creep) {
+var doTransfer = function(targets, creep, res = RESOURCE_ENERGY) {
 	if (targets.length) {
-		if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+		if (creep.transfer(targets[0], res) == ERR_NOT_IN_RANGE){
 			creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff00ff'}});  
 			return true;
 		} 
@@ -9,9 +9,9 @@ var doTransfer = function(targets, creep) {
 	return false;
 }
 
-var doWithdraw = function(creep, targets) {
+var doWithdraw = function(creep, targets, res = RESOURCE_ENERGY) {
 	if (targets.length) {
-		if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+		if (creep.withdraw(targets[0], res) == ERR_NOT_IN_RANGE){
 			creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff00ff'}});  
 			return true;
 		} 
@@ -23,6 +23,7 @@ module.exports = {
     /** @param {Creep} creep **/
     run: function(creep) {
 		var targets;
+		var res;
 
 	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.building = false;
@@ -34,6 +35,14 @@ module.exports = {
 	    }
 
 	    if(creep.memory.building) { 
+			res = _.filter(creep.store.keys(), (res) => (res != RESOURCE_ENERGY && creep.store[res] != 0));
+
+			if (res.length) {
+				targets = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (targets) => { return (targets.structureType == STRUCTURE_CONTAINER && 
+																						   targets.store.getFreeCapacity() > 0)}});
+				doTransfer([targets], creep, res[0])
+			}
+
 			targets = creep.room.find(FIND_STRUCTURES, {filter: (targets) => { return (targets.structureType == STRUCTURE_SPAWN && 
 																						   targets.store.getFreeCapacity(RESOURCE_ENERGY) > 0)}});
 			if (doTransfer(targets, creep)) {return;}
