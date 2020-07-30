056b381f3ce44306a9a8b8be58f750ea165f07c5
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-30 11:10:39 +0800

    runner fix

--- R_runner.js
+++ R_runner.js
@@ -20,7 +20,7 @@ var doWithdraw = function(creep, targets, res = RESOURCE_ENERGY) {
 }
 
 var withdrawAll = function(creep, targets) {
-	if (target && targets.store) {
+	if (targets && targets.store) {
 		res = _.filter(Object.keys(targets.store), (res) => (targets.store[res] != 0)); 
 		if (res.length) {
 			return doWithdraw(creep, targets, res[0]);
