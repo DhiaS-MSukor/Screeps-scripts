8b7540a24e4f34e7ce18ceb51f737dfc1844eaf4
DhiaS-MSukor <48851422+DhiaS-MSukor@users.noreply.github.com>
2020-07-31 12:37:04 +0800

    runner will support its assigned creeps before the last structure type for each working mode

--- R_runner.js
+++ R_runner.js
@@ -72,32 +72,32 @@ module.exports = {
 			if (creep.memory.task == 1) {
 				if (doTransfer(transferStructureTarget(creep, STRUCTURE_EXTENSION), creep)) {return;}
 				if (doTransfer(transferStructureTarget(creep, STRUCTURE_TOWER), creep)) {return;} 
-				if (doTransfer(transferStructureTarget(creep, STRUCTURE_SPAWN), creep)) {return;} 
 
 				targets = transferCreepTarget(creep, 'repairer'); 
 				if (targets) {
 					if (doTransfer(targets, creep)) {return;} 
 				} 
+				if (doTransfer(transferStructureTarget(creep, STRUCTURE_SPAWN), creep)) {return;} 
 			}
 			else if (creep.memory.task == 2) {
 				if (doTransfer(transferStructureTarget(creep, STRUCTURE_TOWER), creep)) {return;}
 				if (doTransfer(transferStructureTarget(creep, STRUCTURE_SPAWN), creep)) {return;} 
-				if (doTransfer(transferStructureTarget(creep, STRUCTURE_EXTENSION), creep)) {return;} 
 
 				targets = transferCreepTarget(creep, 'builder');
 				if (targets) {
 					if (doTransfer(targets, creep)) {return;} 
 				}
+				if (doTransfer(transferStructureTarget(creep, STRUCTURE_EXTENSION), creep)) {return;} 
 			}
 			else {
 				if (doTransfer(transferStructureTarget(creep, STRUCTURE_SPAWN), creep)) {return;} 
 				if (doTransfer(transferStructureTarget(creep, STRUCTURE_TOWER), creep)) {return;} 
-				if (doTransfer(transferStructureTarget(creep, STRUCTURE_EXTENSION), creep)) {return;} 
 				
 				targets = transferCreepTarget(creep, 'builder');
 				if (targets) {
 					if (doTransfer(targets, creep)) {return;} 
 				}
+				if (doTransfer(transferStructureTarget(creep, STRUCTURE_EXTENSION), creep)) {return;} 
 			} 
 	    }
 	    else { 
